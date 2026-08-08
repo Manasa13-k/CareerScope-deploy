98d71750d8a71bb4ebf9aa54a120ad44216bb701
manasalakshmi <korrapatimanasalakshmi@gmail.com>
2026-08-08 22:02:13 +0530

    Remove duplicate health route

--- server/server.js
+++ server/server.js
@@ -1,19 +1,11 @@
 import app from './app.js';
 import connectDB from './config/db.js';
-
 // Connect to MongoDB Database
 connectDB();
-
 const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
-  res.send("CareerScope API is running");
-});
-
 const server = app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
-
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
   console.error(`Unhandled Rejection: ${err.message}`);
