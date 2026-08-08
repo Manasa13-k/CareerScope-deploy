1ffabe09eff3b3244e5c422380ad22af8ddfcde4
manasalakshmi <korrapatimanasalakshmi@gmail.com>
2026-08-08 21:40:48 +0530

    Add API health route

--- server/server.js
+++ server/server.js
@@ -5,7 +5,9 @@ import connectDB from './config/db.js';
 connectDB();
 
 const PORT = process.env.PORT || 5000;
-
+app.get("/", (req, res) => {
+  res.send("CareerScope API is running");
+});
 const server = app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
 });
